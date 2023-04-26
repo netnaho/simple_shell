f335cc60522a13e69efce3aba27c67737182d870
netnaho <87896136+netnaho@users.noreply.github.com>
2023-04-26 14:52:33 -0800

    Create betty.c

---
+++ betty.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("This code will pass the betty check.\n");
+return (0);
+}
